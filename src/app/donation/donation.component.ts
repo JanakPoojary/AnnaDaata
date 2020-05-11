4900332c7961d67283b8e5a5c3f6628d11ebfdbf
JanakPoojary <janakrajpoojari@gmail.com>
2020-05-11 16:13:57 +0530

    bug fixes

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -14,11 +14,23 @@ export class DonationComponent implements OnInit {
   contact:number;
   emailid:string;
   desc:string;
+  view:boolean=false;
+  title:string="Show Form";
   date = new Date();
   constructor(public db: AngularFireDatabase,private router: Router) { 
   }
 
   ngOnInit(): void {
+  }
+  toggle(){
+    if(this.view==false){
+      this.view=true;
+      this.title="Hide Form"
+    }
+    else{
+      this.view=false;
+      this.title="Show Form"
+    }
   }
 add():void{
   this.db.list('users').push({name: this.name, address: this.address, email: this.emailid, contact:this.contact, type:this.type, description:this.desc, date:this.date.toISOString() });
